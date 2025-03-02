6ec61e3133aef32180aa6a093e431945e7b9f470
GedMarc <marc.magon@gmail.com>
2025-03-02 23:36:02 +0200

    Post startup to vertx

--- src/main/java/com/jwebmp/core/base/angular/client/implementations/AngularTypeScriptPostStartup.java
+++ src/main/java/com/jwebmp/core/base/angular/client/implementations/AngularTypeScriptPostStartup.java
@@ -1,17 +1,23 @@
 package com.jwebmp.core.base.angular.client.implementations;
 
+import com.google.inject.Inject;
 import com.guicedee.guicedinjection.interfaces.IGuicePostStartup;
 import com.jwebmp.core.base.angular.client.services.AnnotationHelper;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class AngularTypeScriptPostStartup implements IGuicePostStartup<AngularTypeScriptPostStartup>
 {
+    @Inject
+    private Vertx vertx;
+
     @Override
-    public List<CompletableFuture<Boolean>> postLoad()
+    public List<Future<Boolean>> postLoad()
     {
-        return List.of(CompletableFuture.supplyAsync(() -> {
+        return List.of(vertx.executeBlocking(() -> {
             AnnotationHelper.startup();
             return true;
         }));
